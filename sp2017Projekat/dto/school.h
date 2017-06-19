1afac44d20ac5e66e996c5c89150fe92be20e843
AjdinRahmanovic <ajdin.rahmanovic@fet.ba>
2017-06-19 22:51:25 +0200

    Add files via upload

---
+++ sp2017Projekat/dto/school.h
@@ -0,0 +1,17 @@
+#ifndef SCHOOL_H_
+#define SCHOOL_H_
+
+class School{
+private:
+  //lista za studente
+  //lista za teachere
+  //lista za predmete
+  //lista za odsijeke
+public:
+
+};
+
+
+
+
+#endif
