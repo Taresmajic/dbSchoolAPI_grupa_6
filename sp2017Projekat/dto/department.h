a9d9331e860b570e027433fafa6b8ca849efca20
AjdinRahmanovic <ajdin.rahmanovic@fet.ba>
2017-06-19 19:41:31 +0200

    Update department.h

--- sp2017Projekat/dto/department.h
+++ sp2017Projekat/dto/department.h
@@ -7,7 +7,12 @@ private:
   std::string id_;
   std::string name_;
 public:
+  departments(){}
   departments(const std::string id,const std::string name):id_(id),name_(name){}
+  void setid(const std::string id){id_=id;}
+  void setname(const std::string name){name_=name;}
+  std::string getid()const{return id_;}
+  std::string getname()const{return name_;}
 
 
 };
