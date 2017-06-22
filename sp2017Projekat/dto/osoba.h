d680abef2c3499036e804a06b576f0f2a0f49b01
AjdinRahmanovic <ajdin.rahmanovic@fet.ba>
2017-06-22 12:18:07 +0200

    Update osoba.h

--- sp2017Projekat/dto/osoba.h
+++ sp2017Projekat/dto/osoba.h
@@ -13,6 +13,7 @@ private:
   std::string jmbg_;
 public:
   osoba(){}
+  osoba(const std::string id){id_=id;}
   osoba(const std::string id,const std::string fn,const std::string ln,const std::string bd,const std::string em,const std::string gd,const std::string jmb):id_(id),firstName_(fn),lastName_(ln), birthdate_(bd),email_(em),gender_(gd),jmbg_(jmb){}
   void setId(std::string id){id_=id;}
   std::string getId()const{return id_;}
