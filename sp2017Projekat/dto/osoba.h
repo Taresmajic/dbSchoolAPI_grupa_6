0272d6cd0fc121a82a33c8ab191916a4c5fa9d0f
AjdinRahmanovic <ajdin.rahmanovic@fet.ba>
2017-06-22 13:19:53 +0200

    Update osoba.h

--- sp2017Projekat/dto/osoba.h
+++ sp2017Projekat/dto/osoba.h
@@ -15,8 +15,7 @@ public:
   osoba(){}
   osoba(const std::string id){id_=id;}
   osoba(const std::string id,const std::string fn,const std::string ln,const std::string bd,const std::string em,const std::string gd,const std::string jmb):id_(id),firstName_(fn),lastName_(ln), birthdate_(bd),email_(em),gender_(gd),jmbg_(jmb){}
-  void setId(std::string id){id_=id;}
-  std::string getId()const{return id_;}
+  
   void setId(const std::string id){id_=id;}
   void setfirstName(const std::string fn){firstName_=fn;}
   void setlastName(const std::string ln){lastName_=ln;}
@@ -24,6 +23,7 @@ public:
   void setemail(const std::string em){email_=em;}
   void setgender(const std::string gd){gender_=gd;}
   void setjmbg(const std::string jmb){jmbg_=jmb;}
+  
   std::string getId()const{return id_;}
   std::string getfirstName()const{return firstName_;}
   std::string getlastName()const{return lastName_;}
@@ -31,6 +31,7 @@ public:
   std::string getemail()const{return email_;}
   std::string getgender()const{return gender_;}
   std::string getjmbg()const{return jmbg_;}
+  
   bool operator<(const osoba& drugi){
     return id_<drugi.id_;
   }
