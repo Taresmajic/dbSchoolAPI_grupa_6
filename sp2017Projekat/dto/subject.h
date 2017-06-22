f2a2f479e13b65d15621490e3f6394c083682211
AjdinRahmanovic <ajdin.rahmanovic@fet.ba>
2017-06-22 13:22:52 +0200

    Update subject.h

--- sp2017Projekat/dto/subject.h
+++ sp2017Projekat/dto/subject.h
@@ -4,21 +4,24 @@
 
 class subject{
 private:
-  std::string id_;
+  std::string Id_;
   std::string name_;
   std::string ects_;
   std::string abb_;
 public:
   subject(){}
   subject(const std::string id,const std::string n,const std::string ects,const std::string ab):id_(id),name_(n),ects_(ects),abb_(ab){}
+  
   void setid(const std::string id){id_=id;}
-  void setname(const std::string name){name_=name;}
-  void setects(const std::string ects){ects_=ects;}
-  void setabb(const std::string ab){abb_=ab;}
-  std::string getid()const{return id_;} 
-  std::string getname()const{return name_;}
-  std::string getects()const{return ects_;}
-  std::string getabb()const{return abb_;}
+  void setName(const std::string name){name_=name;}
+  void setEcts(const std::string ects){ects_=ects;}
+  void setAbb(const std::string ab){abb_=ab;}
+  
+  std::string getId()const{return id_;} 
+  std::string getName()const{return name_;}
+  std::string getEcts()const{return ects_;}
+  std::string getAbb()const{return abb_;}
+  
   bool operator<(const subject& drugi){
     return id_<drugi.id_;
   }
