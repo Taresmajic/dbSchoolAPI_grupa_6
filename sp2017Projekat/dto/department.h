cf4e4568df84260ffa1a2632522c30e7b8c9dbe3
AjdinRahmanovic <ajdin.rahmanovic@fet.ba>
2017-06-22 14:51:34 +0200

    Update department.h

--- sp2017Projekat/dto/department.h
+++ sp2017Projekat/dto/department.h
@@ -1,25 +1,28 @@
-#ifndef DEPARTMENTS_H_
-#define DEPARTMENTS_H_
+#ifndef DEPARTMENT_H_
+#define DEPARTMENT_H_
 #include<string>
 
-class departments{
+class department{
 private:
   std::string id_;
   std::string name_;
 public:
-  departments(){}
-  departments(const std::string id,const std::string name):id_(id),name_(name){}
+  department(){}
+  department(const std::string id,const std::string name):id_(id),name_(name){}
+  
   void setid(const std::string id){id_=id;}
   void setname(const std::string name){name_=name;}
+  
   std::string getid()const{return id_;}
   std::string getname()const{return name_;}
-  bool operator<(const departments& drugi){
+  
+  bool operator<(const department& drugi)const{
     return id_<drugi.id_;
   }
-  bool operator>(const departments& drugi){
+  bool operator>(const department& drugi)const{
     return id_>drugi.id_;
   }
-  bool operator==(const departments& drugi){
+  bool operator==(const department& drugi)const{
     return id_==drugi.id_;
   }
 
