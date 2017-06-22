31490b9677dbe1f180555454498ddc9d0fe875e1
Taresmajic <taresmajic@gmail.com>
2017-06-22 12:09:10 +0200

    Update teacher.h

--- sp2017Projekat/dto/teacher.h
+++ sp2017Projekat/dto/teacher.h
@@ -4,11 +4,13 @@
 class teacher : public osoba{
 private:
   std::string departmentId_;
+  std::string title_;
 public:
   teacher(){}
-  teacher(const std::string id,const std::string fn,const std::string ln,const std::string bd,const std::string em,const std::string gd,const std::string jmb,std::string did){
+  teacher(const std::string id,const std::string fn,const std::string ln,const std::string bd,const std::string em,const std::string gd,const std::string jmb,const std::string title,const std::string did){
     osoba(id,fn,ln,bd,em,gd,jmb);
     departmentId_=did;
+    title_=title;
   }
 void setdepartmentId(const std::string id){departmentId_=id;}
 std::string getdepartmentId()const{return departmentId_;}
