b9395be9ac6056b8dba8e0b3bdbf2637ee6fe051
AjdinRahmanovic <ajdin.rahmanovic@fet.ba>
2017-06-22 15:46:25 +0200

    Update teacher.h

--- sp2017Projekat/dto/teacher.h
+++ sp2017Projekat/dto/teacher.h
@@ -11,14 +11,14 @@ public:
   teacher(const std::string id,const std::string fn,const std::string ln,const std::string bd,const std::string em,const std::string gd,const std::string jmb,const std::string title,const std::string did)
    :osoba(id,fn,ln,bd,em,gd,jmb),departmentId_(did),title_(title){};
 
-void setdepartmentId(const std::string id){departmentId_=id;}
+void setDepartmentId(const std::string id){departmentId_=id;}
 void setTitle(const std::string title){title_=title;}
 
 std::string getTitle()const{return title_;}
-std::string getdepartmentId()const{return departmentId_;}
+std::string getDepartmentId()const{return departmentId_;}
 };
-std::ostream& operator <<(std::ostream& out,const teacher& t){
-  out << t.getId() << t.getfirstName() << t.getlastName() << t.getBirthdate() << t.getEmail() << t.getGender() << t.getJmbg() << t.getTitle() << t.getdepartmentId() << std::endl;
+std::ostream& operator << (std::ostream& out,const teacher& t){
+  out << t.getId() << t.getFirstName() << t.getLastName() << t.getBirthdate() << t.getEmail() << t.getGender() << t.getJmbg() << t.getTitle() << t.getdepartmentId() << std::endl;
   return out;
 
 #endif
