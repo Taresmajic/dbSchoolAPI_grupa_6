03635d58251f480f1e0753b4f38f691bdcbd63ef
KenanMazalovic <kenan.mazalovic@fet.ba>
2017-06-23 18:50:54 +0200

    Add files via upload

---
+++ student.h
@@ -0,0 +1,20 @@
+#ifndef STUDENT_H_
+#define STUDENT_H_
+#include "osoba.h"
+
+class student : public osoba{
+private:
+  std::string departmentId_;
+public:
+  student(){}
+  student(const std::string& id):osoba(id){}
+  student(const std::string id,const std::string fn,const std::string ln,const std::string bd,const std::string em,const std::string gd,const std::string jmb,std::string did):osoba(id,fn,ln,bd,em,gd,jmb),departmentId_(did){}
+
+void setDepartmentId(const std::string id){departmentId_=id;}
+
+std::string getDepartmentId()const{return departmentId_;}
+
+};
+
+
+#endif
